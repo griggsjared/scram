42d1889fd8afb6c008d7312d05e51a87c2c8c926
Jared Griggs <griggsjared@gmail.com>
2025-01-16 17:14:25 -0700

    refactor: remove excessive string to rune convertions

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strings"
 	"time"
-	"unicode/utf8"
+
+	"golang.org/x/exp/slices"
 )
 
 const (
@@ -31,48 +31,46 @@ func main() {
 			"Hello, World!",
 			"Hello, from scram",
 			"This is a test",
-      "Cheers! \U0001F37B",
+			"Cheers! \U0001F37B",
 		}
 		phrase = defaultPhrases[rand.Intn(len(defaultPhrases))]
 	}
 
+	//once we have the phrase we want to work with it as a slice of runes
+	rPhrase := []rune(phrase)
+
 	//add the characters from the phrase to the baseChars so that the scrambled phrase can contain the original characters
-	combinedChars := baseChars
-	for i := 0; i < utf8.RuneCountInString(phrase); i++ {
-		char := string([]rune(phrase)[i])
-		if !strings.Contains(combinedChars, string(char)) {
-			combinedChars += string(char)
+	rChars := []rune(baseChars)
+	for i := 0; i < len(rPhrase); i++ {
+		char := rPhrase[i]
+		if !slices.Contains(rChars, char) {
+			rChars = append(rChars, char)
 		}
 	}
 
 	//scramble the phrase
-	var scrambled string
-	for i := 0; i < utf8.RuneCountInString(phrase); i++ {
-		scrambled += string(randomChar(combinedChars))
+	var scrambled []rune
+	for i := 0; i < len(rPhrase); i++ {
+		scrambled = append(scrambled, randomChar(rChars))
 	}
 
 	//while the scrambled phrase does not equal the original phrase, loop through each rune in the phrase and randomly get a new character for that runes index if it did not match
 	//this loop will continue until all characters in the scrambled string match the original string.
-	for scrambled != phrase {
+	for !slices.Equal(rPhrase, scrambled) {
 		time.Sleep(time.Second / time.Duration(baseSpeedFactor))
-		var n []rune
-		cs1 := []rune(phrase)
-		cs2 := []rune(scrambled)
-		for i := 0; i < utf8.RuneCountInString(string(scrambled)); i++ {
-			if cs1[i] != cs2[i] {
-				n = append(n, randomChar(combinedChars))
-			} else {
-				n = append(n, cs1[i])
-			}
-		}
-		scrambled = string(n)
-		fmt.Printf("\r%s", scrambled)
+    for i := range rPhrase {
+      if rPhrase[i] != scrambled[i] {
+        scrambled[i] = randomChar(rChars)
+      } else {
+        scrambled[i] = rPhrase[i]
+      }
+    }
+    fmt.Printf("\r%s", string(scrambled))
 	}
 	fmt.Println()
 }
 
-// randomChar returns a random character from the provided string
-func randomChar(fromChars string) rune {
-	cs := []rune(fromChars)
-	return cs[rand.Intn(len(cs))]
+// randomChar returns a random character from the provided slice of runes
+func randomChar(fromChars []rune) rune {
+	return fromChars[rand.Intn(len(fromChars))]
 }
